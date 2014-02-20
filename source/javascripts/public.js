8a1534676c8e1f1ab848e7225b35a3347411971d
tatelucas <tate.lucas@gmail.com>
2014-02-20 15:57:28 -0500

    nav

--- source/javascripts/public.js
+++ source/javascripts/public.js
@@ -1,29 +1,11 @@
 //= require libs/jquery
 //= require foundation/index
 //= require_tree ./public
-//= require libs/video
+//= require vendor/SVGMagic.min
 
 // Initialize Foundation
 $(document).foundation();
 
-//Big Video for intro
-    _V_("intro_video").ready(function(){
-      
-      // Store the video object
-      var myPlayer = this; 
-      // Make up an aspect ratio
-      var aspectRatio = 264/640; 
-      myPlayer.volume(0);
-
-      function resizeVideoJS(){
-        var width = document.getElementById(myPlayer.id).parentElement.offsetWidth;
-        myPlayer.width(width).height( width * aspectRatio );
-
-      }
-      
-       // Initialize resizeVideoJS()
-      resizeVideoJS();
-      // Then on resize call resizeVideoJS()
-      window.onresize = resizeVideoJS; 
-
-    });
+$(document).ready(function(){
+   $('img').svgmagic();
+});
