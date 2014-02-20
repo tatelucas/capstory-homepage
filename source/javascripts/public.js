90663c0c92a1d538762350a65d82020ac898fca1
tatelucas <tate.lucas@gmail.com>
2014-02-20 10:48:06 -0500

    add bigvideo, jquery for foundation

--- source/javascripts/public.js
+++ source/javascripts/public.js
@@ -1,4 +1,10 @@
+//= require libs/jquery
 //= require foundation/index
 //= require_tree ./public
+//= require libs/video
+//= require libs/imagesloaded.pkgd
+//= require libs/bigvideo/lib/bigvideo
+//
+
 // Initialize Foundation
 $(document).foundation();
