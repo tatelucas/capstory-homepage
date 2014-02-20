c708dd8c08f19a295050325c9d6d0fcb0b461fee
tatelucas <tate.lucas@gmail.com>
2014-02-20 12:49:29 -0500

    video start

--- source/javascripts/public.js
+++ source/javascripts/public.js
@@ -2,9 +2,28 @@
 //= require foundation/index
 //= require_tree ./public
 //= require libs/video
-//= require libs/imagesloaded.pkgd
-//= require libs/bigvideo/lib/bigvideo
-//
 
 // Initialize Foundation
 $(document).foundation();
+
+//Big Video for intro
+    _V_("intro_video").ready(function(){
+      
+      // Store the video object
+      var myPlayer = this; 
+      // Make up an aspect ratio
+      var aspectRatio = 264/640; 
+      myPlayer.volume(0);
+
+      function resizeVideoJS(){
+        var width = document.getElementById(myPlayer.id).parentElement.offsetWidth;
+        myPlayer.width(width).height( width * aspectRatio );
+
+      }
+      
+       // Initialize resizeVideoJS()
+      resizeVideoJS();
+      // Then on resize call resizeVideoJS()
+      window.onresize = resizeVideoJS; 
+
+    });
